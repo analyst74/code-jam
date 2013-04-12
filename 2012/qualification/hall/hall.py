58f4eeb3345dc0a1819ea27047b85879094f7d0c
Bill Yang <zz_fish@hotmail.com>
2013-04-12 17:15:34 -0600

    half done hall of mirror

--- 2012/qualification/hall/hall.py
+++ 2012/qualification/hall/hall.py
@@ -3,7 +3,7 @@
 from collections import deque
 import numpy as np
 
-def run(filename):
+def solve(filename):
     f = open(filename, "r")
     case_count = int(f.readline())
     for i in xrange(case_count):
@@ -16,8 +16,44 @@ def parse_case(line, f):
     h = int(pieces.popleft())
     w = int(pieces.popleft())
     d = int(pieces.popleft())
-    map = np.
-    for i in xrange(h):
-        map
+    # save the map
+    map = [][]
+    for i in range(h):
+        row = f.readline()
+        for j in range(w):
+            map[i][j] = row[j]
+
+    return map, h, w, d
         
 def process_case(case):
+    map, h, w, d = case
+
+    # figure out how many duplications we need for each side
+    hp = d / h / 2 + 1
+    wp = d / w / 2 + 1
+
+    # create mirrors of the map, and combine them
+    fullMap = [[]*(wp*2*w+1) for i in range(hp*2*h+1)]
+    for i in range(hp):
+
+
+
+    # find the center me (among all the me's)
+
+    # find all other me's within d of center me
+    # and create rays between each of them to center me
+
+    # deduct the rays crossing any mirror square
+
+    # deduct stacked rays
+
+    # return remaining number of rays, which is how many reflections we can see
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        filename = "sample.in"
+
+    solve(filename)
